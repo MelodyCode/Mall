4546e7b2ad2fedb027c92119e43753923655c86a
MelodyCode <18826072040@163.com>
2021-12-30 17:42:47 +0800

    环境的设置

---
+++ vue.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+    devServer: {
+        host: 'localhost',
+        port:8000,
+        //代理列表
+        proxy: {
+            'api': {
+                target: 'https://www.imooc.com',//要代理的域名
+                changeOrigin:true,//允许跨域
+                pathRewrite: {
+                    '/api':'' // 这个是定义要访问的路径，名字随便写
+                }
+            }
+        }
+    }
+}
