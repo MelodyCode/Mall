8e2714a00a5ae265fda917c30f015a50f8dd1010
MelodyCode <18826072040@163.com>
2022-01-15 13:03:42 +0800

    退出功能

--- src/App.vue
+++ src/App.vue
@@ -20,6 +20,10 @@ export default {
   mounted() {
     this.getUser();
     this.getCartCount();
+    /*if(this.$cokkie.get('userId')) {
+       this.getUser();
+        this.getCartCount();
+    }*/
   },
   methods:{
     getUser() {
